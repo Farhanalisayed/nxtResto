aac35b9de44e8027067aa6b789bfa9e41bc3eb3c
Farhanalisayed <151043508+Farhanalisayed@users.noreply.github.com>
2024-02-29 18:23:48 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,27 +1,29 @@
-import {Link} from 'react-router-dom'
 import {IoMdCart} from 'react-icons/io'
 import CartContext from '../../context/CartContext'
 import './index.css'
 
-const Header = () => (
-  <CartContext.Consumer>
-    {value => {
-      const {cartList} = value
-      return (
-        <nav className="nav-bar">
-          <div className="header">
-            <h1 className="resto-name">UNI Resto Cafe</h1>
-            <div className="order-cont">
-              <p className="note">My Orders</p>
-              <div className="cart-cont">
-                <IoMdCart className="icon" />
-                <p className="orders-number">{cartList.length}</p>
+const Header = props => {
+  const {restoName} = props
+  return (
+    <CartContext.Consumer>
+      {value => {
+        const {cartList} = value
+        return (
+          <nav className="nav-bar">
+            <div className="header">
+              <h1 className="resto-name">{restoName}</h1>
+              <div className="order-cont">
+                <p className="note">My Orders</p>
+                <div className="cart-cont">
+                  <IoMdCart className="icon" />
+                  <p className="orders-number">{cartList.length}</p>
+                </div>
               </div>
             </div>
-          </div>
-        </nav>
-      )
-    }}
-  </CartContext.Consumer>
-)
+          </nav>
+        )
+      }}
+    </CartContext.Consumer>
+  )
+}
 export default Header
